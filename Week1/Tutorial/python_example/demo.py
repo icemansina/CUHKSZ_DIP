f488861d798c89259fdf2b845c576a9f99ae70d6
wangqin4377 <wangqin4377@qq.com>
2019-01-10 19:38:05 +0800

    modify demo

--- Week1/Tutorial/python_example/demo.py
+++ Week1/Tutorial/python_example/demo.py
@@ -11,18 +11,28 @@ def read(path):
 def save(path, img):
     img = imsave(path, img)
 
-def show(img):
+def show(img,  cmap=None):
     plt.figure()
-    plt.imshow(img)
+    plt.imshow(img, cmap=cmap)
     plt.show()
 
+if __name__ == "__main__":
+    img = read('./rabbit.jpg')
+    #show(img)
+    #print(img)
+    #save('rabbit2.jpg', img)
+    #print(img.shape)
+    img[:,:,0] = np.zeros_like(img[:,:,0])
+    show(img)
 
-img = read('./rabbit.jpg')
-#show(img)
-#print(img)
-#save('rabbit2.jpg', img)
-#print(img.shape)
-img[:,:,0] = np.zeros_like(img[:,:,0])
-show(img)
+def to_binary(img):
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            val = img[i][j]
+            if val < 0.5:
+                img[i][j] = 0
+            else:
+                img[i][j] = 1
+    return img
 
 
