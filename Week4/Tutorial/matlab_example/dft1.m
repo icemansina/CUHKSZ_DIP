d7a1224c696ffa624ca16f12d38d94d67d566cb3
zengbin93 <zeng_bin8888@163.com>
2019-02-21 18:58:04 +0800

    'update'

--- Week4/Tutorial/matlab_example/dft1.m
+++ Week4/Tutorial/matlab_example/dft1.m
@@ -5,11 +5,13 @@ subplot(221)
 imshow(f, [ ])
 title("Raw Image")
 
+% you can use 'ifft2' for 2-D inverse fast Fourier transform 
 F = fft2(f);
 subplot(222)
 imshow(abs(F), [ ])
 title("Fourier Spectrum")
 
+% 'fftshift' is used to shift zero-frequency component to center of spectrum
 Fc = fftshift(F);
 subplot(223)
 imshow(abs(Fc), [ ])
